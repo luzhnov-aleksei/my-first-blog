88876dd22791aa7bf51d84880333d6bd75b8bd8e
luzhnov-aleksei <luzhnov_aleksei@mail.ru>
2021-03-19 20:03:02 +0300

    Second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%)3=!e%gl+ncq!8c-+5wqar(eqfb7buu9^z8fugisg(f2191cg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1' , 'pythonanywhere.com' ]
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', '.pythonanywhere.com' ]
 
 
 # Application definition
